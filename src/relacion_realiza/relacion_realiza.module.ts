a80df28ac681d22264e20d238dcf8b7a8f2823a0
JavierQuinan <javierquiandra@gmail.com>
2024-03-14 17:52:50 -0500

    Fix : Cambios

--- src/relacion_realiza/relacion_realiza.module.ts
+++ src/relacion_realiza/relacion_realiza.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ClienteModule } from '../cliente/cliente.module'; // Ruta relativa al módulo del cliente
-import { PedidoModule } from '../pedido/pedido.module'; // Ruta relativa al módulo del pedido
+import { ClienteModule } from '../cliente/cliente.module'; 
+import { PedidoModule } from '../pedido/pedido.module';
 import { RelacionRealizaController } from './relacion_realiza.controller';
-// Importa y provee el servicio si es necesario
+
 
 @Module({
   imports: [ClienteModule, PedidoModule],
   controllers: [RelacionRealizaController],
-  // providers: [RelacionRealizaService], // Si tienes lógica específica en el servicio
+
 })
 export class RelacionRealizaModule {}
